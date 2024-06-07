7fb06e926a1c16bcd7924aec17ae6e5c88d73923
Bryan Rodrigo Teran Tamayo <bteran@datafast.com.ec>
2024-06-07 12:20:36 -0500

    cambio de nombre sismetic

--- src/test/java/KarateRunner.java
+++ src/test/java/KarateRunner.java
@@ -2,6 +2,6 @@ import com.intuit.karate.junit5.Karate;
 public class KarateRunner {
     @Karate.Test
     Karate testSample() {
-        return Karate.run("classpath:karateTest.feature");
+        return Karate.run("classpath:Sismetic.feature");
     }
 }
